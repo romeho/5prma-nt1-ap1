9ded6b8a91727d96d1181ec2a829062a5aed4bcd
Romero Silva <33010752+romeho@users.noreply.github.com>
2018-09-28 15:42:29 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <section>
+        <form class="form" method="post" action="authentication.php">
+          <fieldset>
+            <p>User<br><input type="text" name="user" placeholder="User"></br></p>
+            <p>Password<br><input type="password" name="password" placeholder="Password"></br></p>
+            <input type="submit" name="submit" value="Submit"><br>
+            <p>
+              <?php
+              if(isset($_SESSION['loginErro'])){
+                echo $_SESSION['loginErro'];
+                unset($_SESSION['loginErro']);
+              }
+              ?>
+            </p>
+          </fieldset>
+        </form>
+      </section>
+    </div>
+  </body>
+</html>
