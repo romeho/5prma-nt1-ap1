4dbc621a86fb475b547a79a402ab3e08e7699bc1
Romero Silva <33010752+romeho@users.noreply.github.com>
2018-10-02 00:04:46 -0300

    romeho

--- consulta.php
+++ consulta.php
@@ -2,7 +2,12 @@
   include 'template.header.php';
  ?>
 
- 
+ <form action="controller.search.php" method="post">
+   <input id="search" type="text" name="search">
+   <label for="search">Procurar</label>
+   <button type="submit" name="action">
+    <i>send</i>
+ </form>
 
 <?php
   include 'template.footer.php';
